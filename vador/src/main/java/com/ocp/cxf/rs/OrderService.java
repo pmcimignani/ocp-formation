605c3319ae4372058e18b5cad798df3692a4f928
Pierre-Mathieu CIMIGNANI <pmc@valuetis.com>
2018-10-04 19:35:52 +0200

    Add SAP Web service CALL

--- vador/src/main/java/com/ocp/cxf/rs/OrderService.java
+++ vador/src/main/java/com/ocp/cxf/rs/OrderService.java
@@ -1,9 +1,11 @@
 package com.ocp.cxf.rs;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -11,10 +13,24 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import javax.xml.ws.Holder;
+
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.transport.http.HTTPConduit;
+import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
+
+import com.sap.document.sap.rfc.functions.ZMATERIALAVAILABILITY;
 
 @Path("/orders")
 public class OrderService {
+	String wsdlUrlCall = "http://frddevci.celesiogroup.com:8042/sap/bc/srt/rfc/sap/zmaterial_availability/400/zmaterial_availability/zmaterial_availability_b";
+
 	final Map<String, OrderProduct> orders;
+	private String userName = "TOTO";
+	private String password = "TITI";
+	final ZMATERIALAVAILABILITY service;
 
 	public OrderService() {
 		orders = new HashMap<String, OrderProduct>();
@@ -28,6 +44,20 @@ public class OrderService {
 		orders.put("1", op);
 		orders.put("2", op);
 
+		final JaxWsProxyFactoryBean jaxWsProxyFactoryBean = new JaxWsProxyFactoryBean();
+		jaxWsProxyFactoryBean.setServiceClass(ZMATERIALAVAILABILITY.class);
+		jaxWsProxyFactoryBean.setAddress(wsdlUrlCall);
+		service = jaxWsProxyFactoryBean.create(ZMATERIALAVAILABILITY.class);
+		final Client client = (Client) ClientProxy.getClient(service);
+		final HTTPConduit http = (HTTPConduit) ((org.apache.cxf.endpoint.Client) client).getConduit();
+		final HTTPClientPolicy httpClientPolicy = new HTTPClientPolicy();
+		http.getAuthorization().setUserName(userName);
+		http.getAuthorization().setPassword(password);
+		httpClientPolicy.setConnectionTimeout(36000);
+		httpClientPolicy.setAllowChunking(false);
+		httpClientPolicy.setReceiveTimeout(32000);
+		http.setClient(httpClientPolicy);
+
 	}
 
 	@GET
@@ -40,10 +70,45 @@ public class OrderService {
 	@GET
 	@Path("/{id}")
 	@Produces("application/json")
-	public Response getOrderById(@PathParam("id") String orderId) {
+	public Response getProductQuantityByIdFromSAP(@PathParam("id") String orderId) {
 		// Simuler une creation d'object
 
-		return Response.ok().entity(orders.get(orderId)).build();
+		// URL wsdlUrlFile = new
+		// URL("http://frddevci.celesiogroup.com:8042/sap/bc/srt/wsdl/flv_10002A111AD1/bndg_url/sap/bc/srt/rfc/sap/zmaterial_availability/400/zmaterial_availability/zmaterial_availability_b?sap-client=400");
+
+		String checkRULE = "RS";
+		BigDecimal enquiryQTY = new BigDecimal(100);
+		String material = orderId;
+		String plant = "180";
+		String stgeLOC = "AFS";
+		String unit = " ";
+		Holder<BigDecimal> replyENQUIRYQTY = new Holder<BigDecimal>();
+		Holder<String> replyERRORDETAIL = new Holder<String>();
+		Holder<String> replyRETURNCODE = new Holder<String>();
+		service.zbapiMATERIALAVAILABILITY(checkRULE, enquiryQTY, material, plant, stgeLOC, unit, replyENQUIRYQTY,
+				replyERRORDETAIL, replyRETURNCODE);
+
+		BigDecimal quantity = replyENQUIRYQTY.value;
+
+		return Response.ok().entity(quantity.toString()).build();
+	}
+
+	@POST
+	@Path("/enregistrer")
+	@Produces("application/json")
+	public Response addProduct(OrderProduct orderProduct) {
+		// Simuler une creation d'object
+		orders.put(orderProduct.getOrderId(), orderProduct);
+		return Response.ok().entity(orderProduct).build();
+	}
+
+	@DELETE
+	@Path("/delete")
+	@Produces("application/json")
+	public Response delProduct(OrderProduct orderProduct) {
+		// Simuler une creation d'object
+		orders.remove(orderProduct.getOrderId());
+		return Response.ok().entity(orderProduct).build();
 	}
 
 	@GET
