8d5bd0325e47b1af9a5bfea38111895f04fba3fb
pmcimignani <pmc@valuetis.com>
2018-10-15 10:55:11 +0200

    Update AppTest.java
    Allow runtime Test

--- luke/src/test/java/fr/ocp/legacy/formation/luke/AppTest.java
+++ luke/src/test/java/fr/ocp/legacy/formation/luke/AppTest.java
@@ -12,16 +12,15 @@ public class AppTest {
 
 	@Test
 	public void testRuntimeSerialisation() throws MalformedURLException {
-		// final URL wsdlLocation = new
-		// URL("http://localhost:8082/serialisation/ref?wsdl");
-		// final RefProductLogicService serviceWSDL = new
-		// RefProductLogicService(wsdlLocation);
-		//
-		// RefProduct refProductService = serviceWSDL.getRefProductLogicPort();
-		// RefRequest refRequest = new RefRequest();
-		// refRequest.setIdProduct(123L);
-		// Assert.assertEquals(7, refProductService.getAll().size());
-		Assert.assertTrue(Boolean.TRUE);
+		 final URL wsdlLocation = new
+		 URL("http://localhost:8082/serialisation/ref?wsdl");
+		 final RefProductLogicService serviceWSDL = new
+		 RefProductLogicService(wsdlLocation);
+		 RefProduct refProductService = serviceWSDL.getRefProductLogicPort();
+		 RefRequest refRequest = new RefRequest();
+		 refRequest.setIdProduct(123L);
+		 Assert.assertEquals(7, refProductService.getAll().size());
+		 Assert.assertTrue(Boolean.TRUE);
 
 	}
 }
